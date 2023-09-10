02760e7a0d5a3b368690a6509f195eeea5ee6541
Jose-Canel-Cargo <jcanellas@hawkmail.hccfl.edu>
2023-09-10 15:29:21 -0400

    first commit

---
+++ src/App.java
@@ -0,0 +1,9 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        int count = 0;
+        while (count < 5) {
+            System.out.println("Welcome to Java");
+            count++;
+        }
+    }
+}
